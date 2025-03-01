3f7c9f909b1bb9e8a109e856bf41c94c5cb03b3c
Aidan Hopper <aidanhop1@gmail.com>
2025-03-01 12:16:28 -0800

    fix

--- website/backend/server.js
+++ website/backend/server.js
@@ -25,7 +25,7 @@ app.post("/api/v1/auth/google/:code", async (req, res) => {
                 grant_type: "authorization_code",
                 code,
                 client_id: process.env.GOOGLE_CLIENT_ID,
-                client_secret: process.env.GOOGLE_SECRET_ID,
+                client_secret: process.env.GOOGLE_CLIENT_SECRET,
                 redirect_uri: "https://tunnl.app/login",
             })
         );
