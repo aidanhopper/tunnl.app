a30e8b406260fae66d0c8607ad63a9a872e6d55a
Aidan Hopper <aidanhop1@gmail.com>
2025-03-23 21:05:53 -0700

    fix

--- daemon/tunneler.py
+++ daemon/tunneler.py
@@ -67,12 +67,7 @@ class Tunneler:
     def stop(self):
         if not self.is_running():
             return
-        if self.is_win:
-            self.process.send_signal(signal.CTRL_BREAK_EVENT)
-        else:
-            os.killpg(os.getpgid(self.process.pid), signal.SIGTERM)
         self.process.terminate()
-        self.process.wait(timeout=5)
         self.process = None
 
     def is_running(self):
