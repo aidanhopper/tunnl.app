0874c5b4d190d5f8ae95ce3ef51bcb0057ba2985
Aidan Hopper <aidanhop1@gmail.com>
2025-04-06 13:43:42 -0700

    www redirect

--- tunnl.app/next.config.ts
+++ tunnl.app/next.config.ts
@@ -1,8 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-
+    /* config options here */
+    async redirects() {
+        return [
+            {
+                source: '/',
+                has: [
+                    {
+                        type: 'host',
+                        value: 'www.tunnl.app',
+                    },
+                ],
+                destination: 'https://tunnl.app',
+                permanent: true,
+            }
+        ]
+    }
 };
 
 export default nextConfig;
