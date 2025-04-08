2995c15c934c4be05944c416579ea0e7b9c453ed
Aidan Hopper <aidanhop1@gmail.com>
2025-04-08 14:55:09 -0700

    traefik api first version

--- traefik-api/src/index.ts
+++ traefik-api/src/index.ts
@@ -1,33 +1,43 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import fs from 'fs/promises';
 
 const app = express();
 
 const port = process.env.PORT || 4000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.json({
-        http: {
-            routers: {
-                whoamiRouter: {
-                    rule: 'HOST(`who.am.i`)',
-                    service: 'whoamiService',
-                    entryPoints: ['websecure'],
-                    // tls: true
-                }
-            },
-            services: {
-                whoamiService: {
-                    loadBalancer: {
-                        servers: [
-                            {
-                                url: 'http://whoami:80'
-                            }
-                        ]
-                    }
-                }
-            }
-        }
-    });
+app.use(express.json());
+
+const authenticate = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token) throw new Error('Unauthorized');
+        if (token !== process.env.TRAEFIK_API_TOKEN) throw new Error('Unauthorized');
+        next();
+    } catch (err) {
+        console.error(err);
+        res.status(401).json({ message: 'Unauthorized' });
+    }
+}
+
+app.post('/', authenticate, async (req: Request, res: Response) => {
+    try {
+        if (!req.body) throw new Error('Request requires body');
+        console.log(req.body);
+        await fs.writeFile('dynamic.json', JSON.stringify(req.body));
+        res.status(201).json({ message: 'Success' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Something went wrong' });
+    }
+});
+
+app.get('/', authenticate, async (req: Request, res: Response) => {
+    try {
+        res.json(JSON.parse(await fs.readFile('dynamic.json', { encoding: 'utf-8' })));
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Something went wrong' });
+    }
 });
 
 app.listen(port, () => {
