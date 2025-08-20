8f7d68c71da56cf226559e18ebd0f7148a03668f
Aidan Hopper <aidanhop1@gmail.com>
2025-08-19 22:33:35 -0700

    researching solutions

--- ziti-private-https/src/app.cpp
+++ ziti-private-https/src/app.cpp
@@ -19,6 +19,22 @@ App &App::getInstance()
     return instance;
 }
 
+/*
+ *  Looked into grabbing bind & dial services with Zitilib and couldn't find solution.
+ *  Next option is to query the Ziti API preferably using the identity certs.
+ *
+ *  The Ziti identity JSON contains all the certificates needed to authenticate
+ *  with the client API, so I just need to read that and make my requests.
+ *  Then I can query services in the context of the identity I've authenticated
+ *  without going through the Ziti C SDK.
+ *
+ *  Then I can wrap the Ziti client API functionality and Zitilib functionality
+ *  up into a nice Ziti class.
+ *
+ *  May want to handle enrollment as well since its made easy with Zitilib.
+ *
+ */
+
 void App::run()
 {
     int rc;
@@ -55,5 +71,6 @@ void App::run()
         std::cout << caller << std::endl;
     } while (true);
 
-    Ziti_close(sock);
+    Ziti_lib_shutdown();
+
 }
--- ziti-private-https/src/main.cpp
+++ ziti-private-https/src/main.cpp
@@ -1,5 +1,13 @@
 #include "app.h"
 
+/*
+ * Idea behind this program
+ * This program is meant to be a reverse proxy for https ziti services.
+ * It does the TLS handshake, and routes to the corresponding backend service.
+ * The reason to do this is so the domain owner can control the certficates
+ * used to encrypt traffic, while allowing other users to use the certs. 
+*/
+
 int main(int argc, char **argv)
 {
     App &app = App::getInstance();
